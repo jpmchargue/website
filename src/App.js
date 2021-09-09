7b0170ec34be2daf88c3028499e37d1ca9a1cf52
jpmchargue <jpmchargue@gmail.com>
2021-09-09 13:59:06 -0400

    First commit, ThreeJS tests.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import * as THREE from "three";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      yeet: "yeet! :P"
+    };
+  }
+
+  componentDidMount() {
+    var width = 8;
+    var height = width * window.innerHeight / window.innerWidth;
+
+    var scene = new THREE.Scene();
+    var camera = new THREE.OrthographicCamera(-width, width, height, -height, 0.1, 2000);
+    //const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    this.mount.appendChild(renderer.domElement);
+
+    const locations = [
+      [0, 0], [0, 1], [1, 0], [2, 0], [2, 1], [2, 2], [2, 3], [2, 4]
+    ];
+    var cubes = [];
+
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshStandardMaterial({color: 0xffffff});
+    for (var l of locations) {
+      var cube = new THREE.Mesh(geometry, material);
+      cube.position.set(l[0], l[1], 0);
+      cubes.push(cube);
+      scene.add(cube);
+    }
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.position.set(-0.2, 2, 1);
+    dirLight.target = cube;
+
+    const ambLight = new THREE.AmbientLight(0xffffff, 0.1);
+
+    const lightHelper = new THREE.PointLightHelper(dirLight);
+
+    scene.add(dirLight, ambLight, lightHelper);
+
+
+    //camera.position.set(-2.1, 2.1, 2.1);
+    const cameraDistance = 4;
+    camera.position.set(-4, 4, 4);
+    camera.rotation.set(-Math.PI/5, -Math.PI/4, 0, 'YXZ');
+    //camera.rotation.set(-Math.PI/2, 0, 0);
+    //camera.rotation.set(-Math.PI/4, -Math.PI/4, Math.PI/4);
+
+    //var rotateSpeed = 0.002;
+    var loop = function () {
+      requestAnimationFrame(loop);
+
+      //camera.rotation.y -= 0.01;
+      //cube.rotation.x += rotateSpeed;
+      //cube.rotation.y += rotateSpeed;
+      renderer.render( scene, camera );
+    };
+    loop();
+
+  }
+
+  render() {
+    return (
+      <div id="name" ref={ref => (this.mount = ref)} />
+    );
+  }
 }
 
 export default App;
